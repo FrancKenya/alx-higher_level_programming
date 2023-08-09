b0d1d1b8425d672ed3c9ff7333705b30fa1c98c1
francisgwaihiga <francisgwaihiga@gmail.com>
2023-08-09 21:08:20 +0000

    syntax error removed

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+def islower(c):
 
-if ord('a') <= ord(c) <= ord('z'):
-    return True
-else:
-    return False
+    if ord('a') <= ord(c) <= ord('z'):
+        return True
+    else:
+        return False
