e98e1eb13a4f155e39f398b3a1b72c4b50b53468
francisgwaihiga <francisgwaihiga@gmail.com>
2023-08-09 22:19:32 +0000

    permission error solved

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@
 def uppercase(s):
 
     for char in s:
-        if ord(char) >= ('a') and ord(char) <= ('z'):
+        if ord(char) >= 97 and ord(char) <= 122:
             char = chr(ord(char) - 32)
-        print("{}".format(char), end=" ")
+        print("{}".format(char), end="")
     print()
