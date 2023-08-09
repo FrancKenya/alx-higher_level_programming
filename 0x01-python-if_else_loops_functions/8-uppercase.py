b7460577b2e8648e9db6db73a9cd2ad01796f359
francisgwaihiga <francisgwaihiga@gmail.com>
2023-08-09 22:12:20 +0000

    Task 8 syntax correction

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@
 def uppercase(s):
 
     for char in s:
-        if ord(char) >= ('a') and ord(char) <= ord('z'):
+        if ord(char) >= ('a') and ord(char) <= ('z'):
             char = chr(ord(char) - 32)
         print("{}".format(char), end=" ")
     print()
