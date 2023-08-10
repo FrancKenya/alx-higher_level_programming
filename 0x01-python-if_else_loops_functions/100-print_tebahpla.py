fd3a130e168933f8712e2d3ff897588bc36a329f
francisgwaihiga <francisgwaihiga@gmail.com>
2023-08-10 01:10:53 +0000

    Task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for values in range(ord('z'), ord('a'), -1):
+for values in range(ord('Z'), ord('a'), -1):
     print("{}".format(chr(values)), end="")
     if values % 2 == 0:
         values += 32
