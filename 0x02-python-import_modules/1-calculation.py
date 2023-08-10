50767de25b901f2f4526998fa1ba807a57155c08
francisgwaihiga <francisgwaihiga@gmail.com>
2023-08-10 19:31:32 +0000

    Task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    mul_results = mul(a, b)
+    div_results = div(a, b)
+    add_results = add(a, b)
+    sub_resutls = sub(a, b)
+
+    print("{} + {} = {}".format(a, b, add_results))
+    print("{} - {} = {}".format(a, b, sub_results))
+    print("{} * {} = {}".format(a, b, mul_results))
+    print("{} / {} = {}".format(a, b, div_results))
