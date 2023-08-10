4cb6a68f9d68e9e9904a2ccf2319caf9764d9609
francisgwaihiga <francisgwaihiga@gmail.com>
2023-08-10 18:35:37 +0000

    Task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from add_0 import add
+
+    a = 1
+    b = 2
+
+    add_results = add(a, b)
+
+    print("{} + {} = {}".format(a, b, add_results))
