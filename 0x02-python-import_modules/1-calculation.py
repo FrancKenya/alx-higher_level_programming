03a7c95e877e662917279a026d5f1ef91ec4c68e
francisgwaihiga <francisgwaihiga@gmail.com>
2023-08-10 19:51:50 +0000

    Output error corrected

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    mul_results = mul(a, b)
-    div_results = div(a, b)
     add_results = add(a, b)
     sub_resutls = sub(a, b)
+    mul_results = mul(a, b)
+    div_results = div(a, b)
 
     print("{} + {} = {}".format(a, b, add_results))
     print("{} - {} = {}".format(a, b, sub_results))
