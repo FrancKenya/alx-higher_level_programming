d9521654753412f1ddd111f178a6c6c11a22adf0
francisgwaihiga <francisgwaihiga@gmail.com>
2023-08-15 00:08:08 +0000

    Task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" Returns a tuple with the length of a string and its first character """
+
+
+def multiple_returns(sentence):
+    if sentence == 0:
+        return (0, None)
+    return (len(sentence), sentence[0])
