258d2068b10cc71e2709b068c988a931eaec9ceb
francisgwaihiga <francisgwaihiga@gmail.com>
2023-08-16 23:53:08 +0000

    Task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Returns a key with the biggest integer value """
+
+
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return (None)
+    i = (max(a_dictionary, key=a_dictionary.get))
+    return (i)
