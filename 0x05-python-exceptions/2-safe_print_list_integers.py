d8bda6a631e7d922d517071018631eff02abe84d
francisgwaihiga <francisgwaihiga@gmail.com>
2023-08-28 20:08:20 +0000

    Task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" function that prints the first x """
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    cnt = 0
+
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            cnt += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return (cnt)
