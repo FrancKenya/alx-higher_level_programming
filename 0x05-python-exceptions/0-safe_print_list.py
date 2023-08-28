0dd08ea26064398714a801fd7b5c306099c066e3
francisgwaihiga <francisgwaihiga@gmail.com>
2023-08-28 15:21:27 +0000

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+""" prints x elements of a list """
+
+
+def safe_print_list(my_list=[], x=0):
+    cnt = 0
+
+    try:
+        for itr in range(x):
+            print(my_list[itr], end="")
+            cnt += 1
+    except IndexError:
+        pass
+    print()
+    return (cnt)
