84d30a351c9a03d16cc6c676f05c43011aecc5ab
francisgwaihiga <francisgwaihiga@gmail.com>
2023-08-29 22:14:27 +0000

    Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""" defines a square """
+
+
+class Square:
+    """ square representation """
+    def __init__(self, size=0):
+        """ initialization """
+        self.size = size
+
+    @property
+    def size(self):
+        """ set size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
