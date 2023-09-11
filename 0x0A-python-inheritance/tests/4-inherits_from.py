95f1393cd4eda34bdb55ae216956ca5ebc5a39d5
FrancKenya <francisgwaihiga@gmail.com>
2023-09-12 02:04:24 +0300

    Task 4

---
+++ 0x0A-python-inheritance/tests/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+""" checks if bject is an instance of a class is inherited """
+
+
+def inherits_from(obj, a_class):
+    """
+    Checks for instance of a class inherited from specified classrgs:
+    Args:
+        Obj: one being compared
+        a_class: One compared with
+    Returns: True or false
+    """
+    return issubclass(type(obj), a_class)
