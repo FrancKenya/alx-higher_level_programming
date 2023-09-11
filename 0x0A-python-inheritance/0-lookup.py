68d87d75d07ca1bad0f47a87ffe84c3250740f3f
FrancKenya <francisgwaihiga@gmail.com>
2023-09-12 00:58:59 +0300

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" Defines a function that returns attributes and methods of object """
+
+
+def lookup(obj):
+    """ Returns the list of attributes and methods of object available """
+    return (dir(obj))
