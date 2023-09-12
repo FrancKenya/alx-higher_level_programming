c516df43a5636602b3a54b42ddc7248887638b8b
FrancKenya <francisgwaihiga@gmail.com>
2023-09-12 11:29:26 +0300

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" class BaseGeometry """
+
+
+def area(self):
+    """
+    A class BaseGeometry
+    Raises:
+        Exception: area is not implemented
+    """
+    raise Exception("area() is not implemented")
