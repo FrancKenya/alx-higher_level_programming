58a25481f3fb6197993b1a0f65db12da6e0d106f
FrancKenya <francisgwaihiga@gmail.com>
2023-09-12 12:16:33 +0300

    Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+""" Defines BaseGeometry class """
+
+
+class BaseGeometry:
+    """ class representing the BaseGeometry """
+
+    def area(self):
+        """ Raises the exception """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates the value
+        Args:
+            name: The name of the argument
+            value: the value being validated
+        Raises:
+            TypeError: if not an integer
+            ValueError: if number is negative or 0
+        """
+
+        if not isinstance(value, int):
+            raise TypeError(f"{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError(f"{} must be greater than 0".format(value))
