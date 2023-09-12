256d114e1d82f53e31d8744bd31602d7e804da4e
FrancKenya <francisgwaihiga@gmail.com>
2023-09-12 22:08:19 +0300

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -13,5 +13,3 @@ def read_file(filename=""):
         with open(filename, encoding="utf-8") as file:
             for content in file:
                 print(content, end="")
-    except FileNotFoundError:
-        pass
