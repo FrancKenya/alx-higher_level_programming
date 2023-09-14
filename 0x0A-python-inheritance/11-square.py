ad2c7b9a3d2701b8725e06e9fc84ad12aa89484f
FrancKenya <francisgwaihiga@gmail.com>
2023-09-14 03:02:46 +0300

    Task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -15,8 +15,9 @@ class Square(Rectangle):
         Args:
             size: The size of the new square.
         """
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
-        return f"[Square] {self.__size}/{self.__size}"
+        return (f"[Square] {self.__size}/{self.__size}")
