506a1cb4d1a3266d0d7f540bbe158e9fc3e825ed
FrancKenya <francisgwaihiga@gmail.com>
2023-09-18 20:38:26 +0300

    Task 9

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -105,9 +105,9 @@ class Rectangle(Base):
                 self.id, self.x, self.y, self.width, self.height)
         return (i)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """
-        Update attributes using no-keyword arguments
+        Update attributes using both args and kwargs
         Args:
            1st argument: id attribute
            2nd argument: width attribute
@@ -115,14 +115,11 @@ class Rectangle(Base):
            4th argument: x attribute
            5th argument: y attribute
         """
-        num_args = len(args)
-        if num_args >= 1:
-            self.id = args[0]
-        if num_args >= 2:
-            self.width = args[1]
-        if num_args >= 3:
-            self.height = args[2]
-        if num_args >= 4:
-            self.x = args[3]
-        if num_args >= 5:
-            self.y = args[4]
+        if args:
+            names = ["id", "width", "height", "x", "y"]
+            for i, j in enumerate(args):
+                if i < len(names):
+                    setattr(self, names[i], j)
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
