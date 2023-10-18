87438b72539f2063e1f404bad9b477ec542bcd15
Francis Waihiga <francisgwaihiga@gmail.com>
2023-10-18 21:30:27 +0000

    Task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -6,9 +6,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Switches to the hbtn_0d_usa database
 USE hbtn_0d_usa;
 
--- Checks if the table states already exists in that data base
-SELECT 1 FROM information_schema.tables WHERE table_name = 'states' LIMIT 1;
-
 -- create it if it does not exist
 CREATE TABLE IF NOT EXISTS states (
 	id INT UNIQUE NOT NULL AUTO_INCREMENT,
