50c95cb414fa3fa4fcd2ed01903242cf79071a06
Francis Waihiga <francisgwaihiga@gmail.com>
2023-10-19 00:41:25 +0000

    Task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Importing the database dump from hbtn_0d_tvshows to my MySQL server
 
-SELECT tv_genres.genre AS 'genre', COUNT(tv_show_genres.tv_show_id) AS number_of_shows
-FROM tv_show_genres
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
+FROM tv_genres
+RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
