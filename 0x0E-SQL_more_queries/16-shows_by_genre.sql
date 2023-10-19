1e4dcffd981ee4577f286a08f708e11461e56e6e
Francis Waihiga <francisgwaihiga@gmail.com>
2023-10-19 01:11:09 +0000

    Task 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- a script that lists all shoiws, and all genres linked to that show, from the database hbtn_0d_tvshows.
 
-SELECT tv_shows.title, name
+SELECT title, name
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title, name;
+ORDER BY title, name;
