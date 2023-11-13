3998b0f8caf7a12185cc576b54ce9aaeb841309e
Francis Waihiga <francisgwaihiga@gmail.com>
2023-11-13 22:11:29 +0000

    Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,12 +2,10 @@
 
 const argsCount = process.argv.length;
 
-if ((argsCount - 2)  === 0) {
-	console.log('No argument');
-}
-else if ((argsCount - 2) === 1) {
-	console.log('Argument found');
-}
-else {
-	console.log('Arguments found');
+if ((argsCount - 2) === 0) {
+  console.log('No argument');
+} else if ((argsCount - 2) === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
