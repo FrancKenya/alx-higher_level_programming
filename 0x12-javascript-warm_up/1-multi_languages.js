0876f67f84c99350a997afe4055291d62c366a16
Francis Waihiga <francisgwaihiga@gmail.com>
2023-11-13 20:41:31 +0000

    Task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+/*
+ * A script that prints 3 lines:
+ * The first line: “C is fun”
+ * The second line: “Python is cool”
+ * The third line: “JavaScript is amazing”
+ */
+
+const firstLine =  'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine =  'JavaScript is amazing';
+
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
