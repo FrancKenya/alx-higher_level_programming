a5737648a50d079957b7222ee9a608d956ab4beb
Francis Waihiga <francisgwaihiga@gmail.com>
2023-11-14 01:31:10 +0000

    Task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -10,15 +10,8 @@
  */
 
 function add (a, b) {
-	  return a + b;
+  console.log(a + b);
 }
-
 const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.argv[3]);
-
-if (!isNaN(arg1) && !isNaN(arg2)) {
-	const result = add(arg1, arg2);
-	console.log(result);
-} else {
-	  console.log('Invalid input. Please provide two integers.');
-}
+add(Number(arg1), Number(arg2));
