8e399e973a65d5031f2cd659af97ff101331c945
Francis Waihiga <francisgwaihiga@gmail.com>
2023-11-15 01:25:24 +0000

    Task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,12 +10,12 @@ class Rectangle {
 
   print () {
     if (!this.width || !this.height) {
-      console.log(''); // If width or height is missing or zero, print an empty line
+      console.log(''); // Handle width or height beingzero by printing an empty line
       return;
     }
-    const row = 'X'.repeat(this.width); // Create a row of X characters with width
+    const row = 'X'.repeat(this.width);
     for (let i = 0; i < this.height; i++) {
-      console.log(row); // Print the row 'height' times to form a rectangle
+      console.log(row);
     }
   }
 }
