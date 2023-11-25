c71eac7f30ce7a6598cb8f525aa298cc89ff7ea0
francis waihiga <francisgwaihiga@gmail.com>
2023-11-25 02:20:08 +0000

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,26 +5,16 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    try:
-        ldb = MySQLdb.connect(
-                host="localhost",
-                user=sys.argv[1],
-                passwd=sys.argv[2],
-                db=sys.argv[3],
-                port=3306
-                )
-        cur = ldb.cursor()
-        query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-        cur.execute(query)
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-            cur.close()
-            ldb.close()
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL:", e)
-        sys.exit(1)
+    db = MySQLdb.connect(
+            host="localhost", user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3], port=3306
+            )
+    cur = db.cursor()
+    cur.execute(
+            """SELECT * FROM states
+            WHERE name LIKE BINARY 'N%' ORDER BY states.id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+        cur.close()
+        db.close()
