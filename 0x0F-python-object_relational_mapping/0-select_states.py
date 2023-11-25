bc7d8caeb5d29ef1767d362df2711350f2c50a11
francis waihiga <francisgwaihiga@gmail.com>
2023-11-25 02:02:45 +0000

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,13 +4,13 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    ldb = MySQLdb.connect(
+    db = MySQLdb.connect(
             host="localhost", user=sys.argv[1],
-            passwd=sys.argv[2], ldb=sys.argv[3], port=3306)
+            passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = ldb.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
     for row in rows:
         print(row)
         cur.close()
-        ldb.close()
+        db.close()
