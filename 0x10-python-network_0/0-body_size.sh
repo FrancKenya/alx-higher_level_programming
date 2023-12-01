d007c5898be95bb388ff03c3eea15d8e60ef951b
Francis Waihiga <francisgwaihiga@gmail.com>
2023-12-01 05:59:35 -0800

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # This script takes in A URL, sends a request to that URL, and displays the size of the body of the response
 
-if [ $# -ne 1 ]; then
-	exit 1
-fi
 url=$1
 
 curl -s -o /dev/null -w "%{size_download}" "$url"
