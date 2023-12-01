d7672faebdf252647b255416358bc7f1e8304179
Francis Waihiga <francisgwaihiga@gmail.com>
2023-12-01 05:50:47 -0800

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-# This script takes in A URL, sends a request to that URL,
-# and displays the size of the body of the response
+# This script takes in A URL, sends a request to that URL, and displays the size of the body of the response
 
 if [ $# -ne 1 ]; then
 	exit 1
 fi
 url=$1
-port_number=$2
 
-curl -s -o /dev/null -w "%{size_download}" "$url":"$port_number"
+curl -s -o /dev/null -w "%{size_download}" "$url"
