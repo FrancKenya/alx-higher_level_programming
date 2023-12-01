01ef1ad363669dde02f1c36e85dc72fe30537214
Francis Waihiga <francisgwaihiga@gmail.com>
2023-12-01 09:16:44 -0800

    Requirement error solved

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # A script that sends a request to a URL, and displays the size of the body
-if [ $# -ne 1 ]; then
-	exit 1
-fi
-url=$1
-
-size=$(curl -s -o /dev/null -w "%{size_download}" "$url")
-echo "$size"
+echo "$(curl -s -o /dev/null -w "%{size_download}" "$1")"
