9664b7931f9e9b78a094d302913adacf5a3446f9
francis waihiga <francisgwaihiga@gmail.com>
2023-12-02 19:51:54 +0000

    Task 2 - documentation

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,10 +10,14 @@ import sys
 
 
 def post_request(url, email):
+    # use parse.urlencode to encode the argument
     data = urllib.parse.urlencode({'email': email})
+    # convert it in to bytes
     data = data.encode('ascii')
+    # make an object request
     req = urllib.request.Request(url, data)
 
+# Use with to open url response and decode
     with urllib.request.urlopen(req) as response:
         undecoded_body = response.read()
         body = undecoded_body.decode('utf-8')
