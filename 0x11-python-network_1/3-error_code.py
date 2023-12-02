b7b7e393f551b961c85accfb4ccd37a224595242
francis waihiga <francisgwaihiga@gmail.com>
2023-12-02 20:41:50 +0000

    Task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+A script that sends a request to the URL and displays the body
+of the response
+Also manages urllib.error.HTTPError exceptions and print Error code followed
+by HTTP status
+Must use with statement
+"""
+import urllib.request
+import sys
+import urllib.error
+
+
+def handle_http_error(url):
+    req = urllib.request.Request(url)
+
+    try:
+        with urllib.request.urlopen(req) as response:
+            body = response.read()
+            dec_body = body.decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as err:
+        print("Error code:", err.code)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    handle_http_error(url)
