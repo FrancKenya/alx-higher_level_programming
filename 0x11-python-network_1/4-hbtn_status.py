14012dcde8382d9e29a855d9f6e3c06f0ca9b675
francis waihiga <francisgwaihiga@gmail.com>
2023-12-02 21:40:26 +0000

    Task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,20 +3,18 @@
 This script fetches the given url and and displays the body of the response
 Only allowed to import requests
 """
-import urllib.request
+import requests
 
 
 def fetch_url():
     url = "https://alx-intranet.hbtn.io/status"
-    # make an object
-    req = urllib.request.Request(url)
-    # use with to make the request and close
-    with urllib.request.urlopen(req) as response:
-        data = response.read()
-        body = data.decode('utf-8')
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.text.encode('utf-8')
+        dec_data = data.decode('utf-8')
         print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
+        print("\t- type:", type(dec_data))
+        print("\t- content:", dec_data)
 
 
 if __name__ == "__main__":
