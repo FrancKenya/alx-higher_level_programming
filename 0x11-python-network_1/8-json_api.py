d165fb6ac62e158981c3bb9ea3d290b0c5d74a64
francis waihiga <francisgwaihiga@gmail.com>
2023-12-03 07:16:36 +0000

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+A scripts that takes a letter and sends a post request to a URL
+with the letter as parameter
+Letter must be sent in variable q
+Handle no argument
+"""
+import sys
+import requests
+
+
+def search_by_letter(letter):
+    url = 'http://0.0.0.0:5000/search_user'
+    data = {'q': letter}
+    response = requests.post(url, data)
+    json_response = response.json
+    if json_response:
+        print("[{}] {}".format(json_response.get(
+            'id'), json_response.get('name')))
+    else:
+        print("No result")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+    search_by_letter(letter)
