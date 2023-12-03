d2e9afe0aca255a43a728009e275f9c0e875677e
francis waihiga <francisgwaihiga@gmail.com>
2023-12-03 08:31:59 +0000

    Task 8 - outout error corrected

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -13,12 +13,15 @@ def search_by_letter(letter):
     url = "http://0.0.0.0:5000/search_user"
     data = {'q': letter}
     response = requests.post(url, data=data)
-    json_response = response.json()
-    if json_response:
-        print("[{}] {}".format(json_response.get(
-            'id'), json_response.get('name')))
-    else:
-        print("No result")
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response.get(
+                'id'), json_response.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
 
 
 if __name__ == "__main__":
