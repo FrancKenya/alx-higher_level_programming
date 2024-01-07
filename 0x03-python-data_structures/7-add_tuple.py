6d3650ccbeaf5c7abe6ffcd0beee8c7637d23a73
francis waihiga <francisgwaihiga@gmail.com>
2024-01-07 14:23:24 +0000

    Task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,6 +5,13 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = (tuple_a[0], 0) if len(tuple_a) < 2 else tuple_a
-    b = (tuple_b[0], 0) if len(tuple_b) < 2 else tuple_b
-    return (a[0] + b[0], a[1] + b[1])
+    tuple_a = tuple_a + (0, 0)
+    tuple_b = tuple_b + (0, 0)
+
+    a1, a2 = tuple_a[:2]
+    b1, b2 = tuple_b[:2]
+
+    sum1 = a1 + b1
+    sum2 = a2 + b2
+
+    return (sum1, sum2)
