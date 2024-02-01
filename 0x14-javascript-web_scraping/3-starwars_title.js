5baf23842ce11161d59b2b8bf395d1aac9be01ba
Francis Waihiga <francisgwaihiga@gmail.com>
2024-02-01 00:59:39 +0000

    Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,18 +6,12 @@
 
 const request = require('request');
 
-if (process.argv.length !== 3) {
-  console.error('Usage: node get-movie-title.js <movie-id>');
-  process.exit(1);
-}
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request.get(apiUrl, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const movieData = JSON.parse(body);
-    console.log(`Title: ${movieData.title}`);
-  } else {
-    console.error('Error:', error || `Status Code: ${response.statusCode}`);
+  if (!error) {
+    const movieData = JSON.parse(body).title;
+    console.log(movieData);
   }
 });
