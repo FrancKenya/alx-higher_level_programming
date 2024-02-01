eb55444b82ee4acd3a62d305007b89e510f90f82
Francis Waihiga <francisgwaihiga@gmail.com>
2024-02-01 13:36:29 +0000

    Task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,16 +6,15 @@ const request = require('request');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request.get(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    fs.writeFile(filePath, body, 'utf-8', (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log('Webpage content successfully stored in the file:', filePath);
-      }
-    });
-  } else {
-    console.error('Error:', error || `Status Code: ${response.statusCode}`);
+function fsCallfunction (err) {
+  if (err) {
+    console.log(err);
   }
-});
+}
+
+function reqCallfunction (error, response, body) {
+  if (!error) {
+    fs.writeFile(filePath, body, 'utf-8', fsCallfunction);
+  }
+}
+request.get(url, reqCallfunction);
