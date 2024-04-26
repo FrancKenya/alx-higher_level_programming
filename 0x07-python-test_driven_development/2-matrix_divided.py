ba50bc467f5f1125e778d0a5667e1f54df86e3fe
Francis Waihiga <francisgwaihiga@gmail.com>
2024-04-26 03:27:23 +0300

    Task 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -19,19 +19,19 @@ def matrix_divided(matrix, div):
         if rows of the matrix are of varying sizes, or if div is not a number.
         ZeroDivisionError: If div is equal to 0.
     """
-    if not (
-        isinstance(matrix, list) or matrix == [] or not all(
-            isinstance(row, list) for row in matrix)
-        or not all((isinstance(
-            element, int) or isinstance(element, float))
-                for element in [num for row in matrix for num in row])):
-        raise TypeError("matrix must be a matrix (list of lists) of"
-                        "integers/floats")
-    if not all(len(row) == len(matrix[0]) for row in matrix):
+    if not all(isinstance(row, list) for row in matrix) or not all(
+            isinstance(num, (int, float)) for row in matrix for num in row):
+        raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+
+    elif len(set(len(row) for row in matrix)) > 1:
         raise TypeError("Each row of the matrix must have the same size")
-    if not isinstance(div, int) and not isinstance(div, float):
+
+    elif not(isinstance(div, (int, float))):
         raise TypeError("div must be a number")
-    if div == 0:
+
+    elif div == 0:
         raise ZeroDivisionError("division by zero")
-    rslt = [list(map(lambda x: round(x / div, 2), row)) for row in matrix]
-    return (rslt)
+
+    rslt = [list(map(lambda num: round(num / div, 2), row)) for row in matrix]
+    return rslt
