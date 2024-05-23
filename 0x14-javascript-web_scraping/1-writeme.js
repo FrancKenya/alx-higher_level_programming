2a80300903801e0ec35212dc2216b700f93985c1
Francis Waihiga <francisgwaihiga@gmail.com>
2024-05-24 02:20:41 +0300

    Task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/* A script that writes a string to a file. */
+
+const fs = require('fs');
+
+function recall (err, data) {
+	if (err) {
+	    console.error(err); // Print the error object if an error occurs
+	}
+}
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', recall);
