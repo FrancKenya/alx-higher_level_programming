c892428d1d18372c0841f78d859efa8caec81f47
Francis Waihiga <francisgwaihiga@gmail.com>
2024-05-23 16:27:17 -0700

    Task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,8 +5,8 @@
 const fs = require('fs');
 
 function recall (err, data) {
-	if (err) {
-	    console.error(err); // Print the error object if an error occurs
-	}
+  if (err) {
+    console.error(err); // Print the error object if an error occurs
+  }
 }
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', recall);
