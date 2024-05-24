b0881ddbc86cbbd34c5d2f1b3ed17ebce0e74835
Francis Waihiga <francisgwaihiga@gmail.com>
2024-05-23 17:29:40 -0700

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,13 +5,13 @@
 const request = require('request');
 
 function reqRecall (err, response, body) {
-	if (!err) {
-            const rslt = JSON.parse(body).rslt;
-            console.log(rslt.reduce((count, movie) => {
-                return movie.characters.find((character) => character.endsWith('/18/'))
-                    ? count + 1
-                    : count;
-	    }, 0));
-	}
+  if (!err) {
+    const rslt = JSON.parse(body).rslt;
+    console.log(rslt.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
 }
 request.get(process.argv[2], reqRecall);
